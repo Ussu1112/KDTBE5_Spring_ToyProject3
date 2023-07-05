e1bd3e80f044203aa5311dfb31b9eb433953ae8a
brotherstone97 <brotherstone97@gmail.com>
2023-07-05 13:39:34 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/fastcampus/group9/toyproject3/Toyproject3Application.java
@@ -0,0 +1,13 @@
+package fastcampus.group9.toyproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toyproject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/fastcampus/group9/toyproject3/Toyproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.group9.toyproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toyproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
