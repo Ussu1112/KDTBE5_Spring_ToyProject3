d8dde6a66da6050f2a0a546b1e81596ebe5171d4
JOOWON <wer07032@naver.com>
2023-07-14 20:27:46 +0900

    feat:Thumbnail Repository 구현
    Issues : #18

---
+++ src/main/java/fastcampus/group9/toyproject3/board/Thumbnail/ThumbnailRepository.java
@@ -0,0 +1,6 @@
+package fastcampus.group9.toyproject3.board.Thumbnail;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
+}
