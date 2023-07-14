67a9974c0ee5b5810fe70b817c16e7dca9fbddc5
JOOWON <wer07032@naver.com>
2023-07-14 20:27:32 +0900

    feat:Board Repository 구현
    Issues : #18

---
+++ src/main/java/fastcampus/group9/toyproject3/board/BoardRepository.java
@@ -0,0 +1,19 @@
+package fastcampus.group9.toyproject3.board;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query(value = "SELECT b FROM Board b WHERE b.title LIKE %:title%")
+    Page<BoardResponse.SelectDTO> findByTitle(String title, Pageable pageable);
+
+    @Query(value = "SELECT b FROM Board b WHERE b.user = :fullName")
+    Page<BoardResponse.SelectDTO> findByFullName(@Param("fullName") String fullName, Pageable pageable);
+
+    @Query(value = "SELECT b FROM Board b WHERE b.content LIKE %:content%")
+    Page<BoardResponse.SelectDTO> findByContent(@Param("content") String content, Pageable pageable);
+}
