4cc18bc105df7058ab1ceeb4164722cb1bb37d9b
HanHyeJi <hjhan.0621@gmail.com>
2023-07-17 04:31:02 +0900

    feat: 회원가입, 로그인 기능을 위한 joinDTO, loginDTO 생성

--- src/main/java/fastcampus/group9/toyproject3/user/UserRequest.java
+++ src/main/java/fastcampus/group9/toyproject3/user/UserRequest.java
@@ -1,8 +1,8 @@
 package fastcampus.group9.toyproject3.user;
 
-
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
@@ -10,46 +10,48 @@ import javax.validation.constraints.Size;
 
 public class UserRequest {
 
-    @Setter
-    @Getter
-    public static class LoginDTO {
-        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
-        @NotEmpty
-        private String username;
-        @NotEmpty
-        @Size(min = 4, max = 20)
-        private String password;
-    }
-
-
+    @ToString
     @Setter
     @Getter
     public static class JoinDTO {
 
-        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
-        @NotEmpty
+        @NotEmpty (message = "이름을 입력해주세요.")
         private String username;
 
-        @NotEmpty
-        @Size(min = 4, max = 20)
+        @NotEmpty (message = "비밀번호를 입력해주세요.")
         private String password;
 
-        @NotEmpty
-        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요")
+        @NotEmpty (message = "이메일을 입력해주세요.")
         private String email;
 
-        @NotEmpty
-        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
-        private String fullName;
+        @NotEmpty (message = "닉네임을 입력해주세요.")
+        private String nickname;
 
         public User toEntity() {
             return User.builder()
                     .username(username)
                     .password(password)
                     .email(email)
-                    .fullName(fullName)
-                    .status(true)
+                    .nickname(nickname)
+                    .role(UserRole.SPROUT)
                     .build();
+        }
+    }
+
+    @ToString
+    @Setter
+    @Getter
+    public static class LoginDTO {
+        @NotEmpty
+        private String username;
+        @NotEmpty
+        private String password;
+
+        public static LoginDTO toLoginDTO(User user) {
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setUsername(user.getUsername());
+            loginDTO.setPassword(user.getPassword());
+            return loginDTO;
         }
     }
 }
