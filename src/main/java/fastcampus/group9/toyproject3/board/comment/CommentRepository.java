a35592acf5f48e9d48c13a9c23bed1270e7cdd4b
brotherstone97 <brotherstone97@gmail.com>
2023-07-16 15:13:37 +0900

    feat: 특정 게시물의 댓글 리스트를 가져오는 Serivce, Repository 메서드 구현
    Issues: #20

--- src/main/java/fastcampus/group9/toyproject3/board/comment/CommentRepository.java
+++ src/main/java/fastcampus/group9/toyproject3/board/comment/CommentRepository.java
@@ -1,7 +1,9 @@
 package fastcampus.group9.toyproject3.board.comment;
 
-import fastcampus.group9.toyproject3.board.comment.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByBoard_Id(Long boardId);
 }
--- src/main/java/fastcampus/group9/toyproject3/board/comment/CommentService.java
+++ src/main/java/fastcampus/group9/toyproject3/board/comment/CommentService.java
@@ -4,9 +4,17 @@ import fastcampus.group9.toyproject3.board.comment.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
 
+    @Transactional
+    public List<Comment> findCommentList(Long boardId){
+        List<Comment>  commentList = commentRepository.findAllByBoard_Id(boardId);
+        return commentList;
+    }
 }
