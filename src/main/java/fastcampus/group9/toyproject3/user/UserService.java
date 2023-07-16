23aba5f4879bfb962186453d0446912994a232f1
HanHyeJi <hjhan.0621@gmail.com>
2023-07-17 04:25:49 +0900

    feat: UserService 구현

--- src/main/java/fastcampus/group9/toyproject3/user/UserService.java
+++ src/main/java/fastcampus/group9/toyproject3/user/UserService.java
@@ -1,14 +1,6 @@
 package fastcampus.group9.toyproject3.user;
 
-import fastcampus.group9.toyproject3._core.erros.exception.Exception400;
-import fastcampus.group9.toyproject3._core.erros.exception.Exception401;
-import fastcampus.group9.toyproject3._core.security.JwtTokenProvider;
-import fastcampus.group9.toyproject3._core.security.MyUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,36 +9,46 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    private final UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
 
-    @Transactional // Springframework Transaction (주의)
-    public UserResponse.JoinDTO 회원가입(UserRequest.JoinDTO joinDTO) {
-        // 1. 동일 유저네임 존재 검사
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(UserRequest.JoinDTO joinDTO) {
+
+        System.out.println("service join 실행");
+
         Optional<User> userOP = userRepository.findByUsername(joinDTO.getUsername());
-        if (userOP.isPresent()) {
-            throw new Exception400("username", "동일한 유저네임이 존재합니다");
+
+        if(userOP.isPresent()){
+            throw new RuntimeException("이미 사용중인 ID 입니다.");
         }
 
-        // 2. 회원가입
-        joinDTO.setPassword(passwordEncoder.encode(joinDTO.getPassword()));
         User userPS = userRepository.save(joinDTO.toEntity());
 
-        // 3. DTO 응답
-        return new UserResponse.JoinDTO(userPS);
+        System.out.println("service/join 성공");
+        System.out.println("userPS = " + userPS);
     }
 
-    @Transactional(readOnly = true) // 변경감지 하지 않음
-    public String 로그인(UserRequest.LoginDTO loginDTO) {
-        try {
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
-                    = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
-            Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
-            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
-            return JwtTokenProvider.create(myUserDetails.getUser());
-        }catch (Exception e){
-            throw new Exception401("인증되지 않았습니다");
+    @Transactional
+    public UserRequest.LoginDTO login(UserRequest.LoginDTO loginDTO) {
+
+        System.out.println("service login 실행");
+
+
+        Optional<User> userOP = userRepository.findByUsername(loginDTO.getUsername());
+        if (userOP.isPresent()) {
+            User user = userOP.get();
+
+            if (user.getPassword().equals(loginDTO.getPassword())) {
+                UserRequest.LoginDTO loginUser = UserRequest.LoginDTO.toLoginDTO(user);
+                return loginUser;
+            } else {
+                throw new RuntimeException("아이디 혹은 패스워드가 틀렸습니다");
+            }
+
+        } else {
+            throw new RuntimeException("아이디 혹은 패스워드가 틀렸습니다");
         }
     }
+
 }
