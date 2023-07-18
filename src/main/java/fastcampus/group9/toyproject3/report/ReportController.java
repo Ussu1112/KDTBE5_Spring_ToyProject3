ed0e25b5dd839a9237566aff552705c481b94285
brotherstone97 <brotherstone97@gmail.com>
2023-07-18 18:42:03 +0900

    feat: 신고 기능 컨트롤러 메서드 구현
    Issues: #20

---
+++ src/main/java/fastcampus/group9/toyproject3/report/ReportController.java
@@ -0,0 +1,24 @@
+package fastcampus.group9.toyproject3.report;
+
+import fastcampus.group9.toyproject3.board.Board;
+import fastcampus.group9.toyproject3.board.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/report")
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportService reportService;
+    private final BoardService boardService;
+
+    @RequestMapping("/{boardId}")
+    public String reportBoard(@PathVariable Long boardId, ReportRequest.CreateDTO report){
+//        Board board = boardService.findBoard(boardId);
+        reportService.saveReport(report.toEntity());
+        return "redirect: /board/list";
+    }
+
+}
