2f35faee9ed7add6a39b9dea87deb8c3c274bd3b
Ussu1112 <ussu1112@naver.com>
2023-07-18 13:33:26 +0900

    feat: #25 공통 예외처리 구현

---
+++ src/main/java/fastcampus/group9/toyproject3/_core/errors/exception/Exception403.java
@@ -0,0 +1,22 @@
+package fastcampus.group9.toyproject3._core.errors.exception;
+
+import fastcampus.group9.toyproject3._core.utils.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+// 권한 없음
+@Getter
+public class Exception403 extends RuntimeException {
+    public Exception403(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.FORBIDDEN;
+    }
+}
