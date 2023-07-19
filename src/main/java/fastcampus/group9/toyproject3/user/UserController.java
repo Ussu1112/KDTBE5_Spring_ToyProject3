f0e1109e5714c5df9e0c53f512bc34d207a71d87
HanHyeJi <hjhan.0621@gmail.com>
2023-07-19 19:34:15 +0900

    feat: 회원정보 페이지 구현

--- src/main/java/fastcampus/group9/toyproject3/user/UserController.java
+++ src/main/java/fastcampus/group9/toyproject3/user/UserController.java
@@ -1,11 +1,15 @@
 package fastcampus.group9.toyproject3.user;
 
+import fastcampus.group9.toyproject3._core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,11 +28,22 @@ public class UserController {
     public String joinForm() {
         return "/joinForm";
     }
+
     @PostMapping("/join")
-    public String join(@Valid UserRequest.JoinDTO joinDTO, Errors errors) {
+    public ResponseEntity<?> join(@Valid UserRequest.JoinDTO joinDTO, Errors errors) {
         System.out.println("controller join 실행");
         UserResponse.JoinDTO responseDTO = userService.join(joinDTO);
-//        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
-        return "redirect:/login";
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+//        return "redirect:/login";
     }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "detailsForm";
+    }
+
+
+
 }
